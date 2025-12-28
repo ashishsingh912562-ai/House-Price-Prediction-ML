d9b1baefaa3f7100fb598930d1e82a97ded173c7
ashishsingh912562-ai <ashishsingh912562@gmail.com>
2025-12-28 15:56:26 +0530

    Update app .py

--- app .py
+++ app .py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
 
 st.title('🏠House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/ec/d3/b6/ecd3b6b355ab6f67c9679c42f585d817.gif')
@@ -16,11 +17,20 @@ st.sidebar.title('🏘️ Select House features')
 st.sidebar.image('https://i.pinimg.com/originals/ec/d3/b6/ecd3b6b355ab6f67c9679c42f585d817.gif')
 all_value = []
 for i in X:
-  min_value = (X[i].min())
-  max_value = (X[i].max())
+  min_value = int(X[i].min())
+  max_value = int(X[i].max())
   ans = st.sidebar.slider(f'Select {i} value',min_value,max_value)
   all_value.append(ans)
 
 #st.write(all_value)
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+final_value = scaler.transform([all_value])
 
+model = RandomForestRegressor()
+model.fit(X,y)
+House_price = model.Predict(final_value)
 
+with st.spinner('Predicting House Price'):
+  time.sleep()
+st.write(house_price)
