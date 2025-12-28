d85c0261d37f876bae2a57542511f7a4e4826c5b
ashishsingh912562-ai <ashishsingh912562@gmail.com>
2025-12-28 15:00:35 +0530

    Update app .py

--- app .py
+++ app .py
@@ -6,3 +6,4 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠House Price prediction using ML')
+st.image('https://i.pinimg.com/originals/ec/d3/b6/ecd3b6b355ab6f67c9679c42f585d817.gif')
